7b5ae2ac3cc2014eaca737ad915134ebaffebc16
misaharitsoa <misaharix>
2026-05-07 14:29:53 +0300

    mon enregistrement local

--- api/controllers/__init__.py
+++ api/controllers/__init__.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from .personnelles.propos.personnelleController import PersonnelleController
 from .auth.login.loginController import LoginController
 from .personnelles.propos.etatCivilController import EtatCivilController
@@ -66,4 +67,76 @@ __all__ = [
     "TypeCongeController",
     "PassationServiceController",
     "UserManagementController"
+=======
+from .personnelles.propos.personnelleController import PersonnelleController
+from .auth.login.loginController import LoginController
+from .personnelles.propos.etatCivilController import EtatCivilController
+from .personnelles.propos.proposController import ProposController
+from .personnelles.propos.sexeController import SexeController
+from .personnelles.propos.CinsController import CinsController
+from .personnelles.propos.photosController import PhotosController
+from .personnelles.banque.agenceController import AgenceController
+from .personnelles.banque.banqueController import BanqueController
+from .personnelles.banque.coordonneesBancaireController import CoordonneesBancaireController
+from .personnelles.contact.contactUrgentController import ContactUrgentController
+from .personnelles.contact.relationController import RelationController
+from .personnelles.fonction.fonctionController import FonctionController
+from .personnelles.fonction.PosteController import PosteController
+from .personnelles.fonction.ServiceController import ServiceController
+from .personnelles.fullController import PersonnelFullController
+from .personnelles.propos.familleController import FamilleController
+from .personnelles.diplome.experienceController import ExperienceController
+from .personnelles.diplome.diplomeController import DiplomeController
+from .personnelles.diplome.formationController import FormationController
+from .personnelles.propos.enfantController import EnfantController
+from .personnelles.fonction.contratController import ContratController
+from .personnelles.fonction.typeContratController import TypeContratController
+from .personnelles.fonction.modefinancementController import ModeFinancementController
+from .role.roleController import RoleController
+from .permission.permissionController import PermissionController    
+from .admin.loginadController import LoginadminController
+from .conge.congeController import CongeController
+from .conge.soldeCongeController import SoldeCongeController
+from .conge.statutController import StatutController
+from .conge.typeCongeController import TypeCongeController
+from .conge.passationServiceController import PassationServiceController
+from .usermanage.usermanageController import UserManagementController
+
+__all__ = [
+    "EtatCivilController",
+    "ProposController",
+    "BanqueController",
+    "AgenceController",
+    "CoordonneesBancaireController",
+    "ContactUrgentController",
+    "RelationController",
+    "FonctionController",
+    "PosteController",
+    "ServiceController",
+    "CinsController",
+    "PersonnelleController",
+    "PhotosController",
+    "SexeController",
+    "PersonnelFullController",
+    "LoginController",
+    "FamilleController",
+    "ExperienceController",
+    "DiplomeController",
+    "FormationController",
+    "EnfantController",
+    "ContratController",
+    "TypeContratController",
+    "ModeFinancementController",
+    "LoginController",
+    "RoleController",
+    "PermissionController",
+    "LoginadminController",
+    "CongeController",
+    "SoldeCongeController",
+    "StatutController",
+    "TypeCongeController",
+    "PassationServiceController",
+    "UserManagementController"
+
+>>>>>>> 23088e43 (mon enregistrement local)
 ]
--- api/dto/conge/congeDto.py
+++ api/dto/conge/congeDto.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from rest_framework import serializers
 from api.models.conge.conge import Conge
 from api.models.propos.personnelles import Personnelles
@@ -43,4 +44,46 @@ class CongeDTO(serializers.ModelSerializer):
             'created_at',
             'updated_at'
         ]
+=======
+from rest_framework import serializers
+from api.models.conge.conge import Conge
+from api.models.propos.personnelles import Personnelles
+from api.models.conge.typeConges import TypeConge
+from api.models.conge.soldeConge import SoldeConge
+from api.models.conge.statut import Statut
+from api.models.conge.passationservice import PassationService  # ← ajouter cet import
+
+
+class CongeDTO(serializers.ModelSerializer):
+    personnel = serializers.PrimaryKeyRelatedField(queryset=Personnelles.objects.all())
+    type_conge = serializers.PrimaryKeyRelatedField(queryset=TypeConge.objects.all())
+    solde_conge = serializers.PrimaryKeyRelatedField(queryset=SoldeConge.objects.all())
+    statut = serializers.PrimaryKeyRelatedField(queryset=Statut.objects.all())
+    nombre_jours = serializers.IntegerField(read_only=True)
+
+    # ✅ AJOUTER CECI
+    passation_service = serializers.PrimaryKeyRelatedField(
+        queryset=PassationService.objects.all(),
+        required=False,
+        allow_null=True
+    )
+
+    class Meta:
+        model = Conge
+        fields = [
+            'id',
+            'personnel',
+            'type_conge',
+            'solde_conge',
+            'date_debut',
+            'date_fin',
+            'nombre_jours',
+            'description',
+            'statut',
+            'passation_service',
+            'validated_by',
+            'created_at',
+            'updated_at'
+        ]
+>>>>>>> 23088e43 (mon enregistrement local)
         read_only_fields = ['nombre_jours', 'created_at', 'updated_at']
