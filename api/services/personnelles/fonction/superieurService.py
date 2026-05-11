580fc4306311bf47f1e486cd815f69a6a3618c9f
Naly <dharianeylan@gmail.com>
2026-05-11 09:38:49 +0300

    superieur 11/05/26 09:38

--- api/services/personnelles/fonction/superieurService.py
+++ api/services/personnelles/fonction/superieurService.py
@@ -1,34 +1,22 @@
-from api.models import Superieur
-from api.dto.personnelles.fonction.superieurDto import SuperieurDto
+from api.models.fonction.fonctions import Fonctions
+from api.models.auth.login.loginModel import Login
 
 class SuperieurService:
-    @staticmethod
-    def create(data)-> Superieur:
-        return Superieur.objects.create(nom=data['nom'])
 
     @staticmethod
-    def getAll()-> list[Superieur]:
-        return Superieur.objects.all().order_by("id")
+    def getAll():
+        """Retourne tous les personnels avec leurs supérieurs"""
+        fonctions = Fonctions.objects.select_related(
+            'personnelle', 'service'
+        ).prefetch_related(
+            'superieurs__personnelle'
+        ).filter(dateFin__isnull=True)
+        return fonctions
+
     @staticmethod
-    def get(id):
-        return Superieur.objects.get(id=id)
-    @staticmethod
-    def getById(id: int)-> Superieur:
-        return Superieur.objects.get(id=id)
-    
-    @staticmethod
-    def update(id: int, nom: str)-> Superieur:
-        superieur = Superieur.objects.get(id=id)
-        superieur.nom = nom
-        superieur.save()
-        return superieur
-    @staticmethod
-    def getByIdDto(id: int)-> SuperieurDto:
-        superieur = SuperieurService.getById(id)
-        return SuperieurDto(superieur)
-    
-    @staticmethod
-    def getAllDto()-> list[SuperieurDto]:
-        
-        superieurs = SuperieurService.getAll()
-        return SuperieurDto(superieurs, many=True) 
+    def update(fonction_id: int, superieurs_ids: list):
+        """Mettre à jour les supérieurs d'une fonction"""
+        fonction = Fonctions.objects.get(id=fonction_id)
+        logins   = Login.objects.filter(id__in=superieurs_ids)
+        fonction.superieurs.set(logins)
+        return fonction
